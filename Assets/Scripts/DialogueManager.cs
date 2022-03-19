e0c8e0e01db89c97ad7f14a38c9cc8038abd1337
MiVladie <justvladon@outlook.com>
2022-03-20 00:06:01 +0200

    added new models

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -71,7 +71,7 @@ public class DialogueManager : MonoBehaviour
 
         yield return new WaitForSeconds(initialDelay);
 
-        for(int i = 0; i < texts[currentText].Length; i++)
+        for(int i = 0; i <= texts[currentText].Length; i++)
         {
             visibleText = texts[currentText].Substring(0, i);
             transform.GetChild(0).GetComponent<Text>().text = visibleText;
